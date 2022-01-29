a23adccd958f0019f4463c66e90e8e7d528032bd
nikhilrana884 <nikhilrana884@gmail.com>
2022-01-29 22:05:47 +0530

    Material ui and google map dependencies added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>
+                Welcome To Travel Advisor
+            </h1>
+        </div>
+    );
+}
+
+export default App;
