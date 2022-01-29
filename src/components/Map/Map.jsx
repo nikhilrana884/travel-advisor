35a1e09bd1dcd031a6a8aebdfad7008181ce8d49
nikhilrana884 <nikhilrana884@gmail.com>
2022-01-29 22:55:28 +0530

    Edited Map.jsx and styes.js

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
-const Map = () => {
-    return (
-        <h1>Map</h1>
-    )
-}
+import useStyles from './styles.js';
+
+const Map = () => { 
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const coordinates = { lat:0 ,lng:0 };
+
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyAhL01DokqAItPXd5DBXpEQ2eHbqeIXsp8' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={ '' }
+        onChange={'' }
+        onChildClick={ '' }
+      >
+
+      </GoogleMapReact>
+    </div>
+  );
+};
+
 export default Map;
